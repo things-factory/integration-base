27385467b659c25b6896342968940927138a9e13
chunseop <chunseop@gmail.com>
2020-01-29 12:00:58 +0900

    init param관련 수정.

--- server/graphql/resolvers/scenario/start-scenario.ts
+++ server/graphql/resolvers/scenario/start-scenario.ts
@@ -2,14 +2,16 @@ import { getRepository } from 'typeorm'
 import { Scenario } from '../../../entities'
 
 export const startScenario = {
-  async startScenario(_: any, { name, data }, context: any) {
+  async startScenario(_: any, { name, data="{}" }, context: any) {
     var repository = getRepository(Scenario)
     var scenario = await repository.findOne({
       where: { domain: context.state.domain, name },
       relations: ['domain', 'steps', 'creator', 'updater']
     })
 
-    await scenario.start(data)
+    await scenario.start({
+      'INIT': JSON.parse(data)
+    })
     await repository.save(scenario)
 
     return scenario
