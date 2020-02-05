4bb00b75ba7ae2d9e16471dd844d29793582c46a
Hearty Oh <heartyoh@hatiolab.com>
2020-02-05 15:40:40 +0900

    sort task-type, connector

--- server/graphql/resolvers/connector/connectors.ts
+++ server/graphql/resolvers/connector/connectors.ts
@@ -3,13 +3,17 @@ import { Connections } from '../../../engine/connections'
 export const connectorsResolver = {
   connectors(_: any, {}, context: any) {
     var connectors = Connections.getConnectors()
-    var items = Object.keys(connectors).map(name => {
-      return {
-        name,
-        description: '',
-        parameterSpec: connectors[name].parameterSpec
-      }
-    })
+    var items = Object.keys(connectors)
+      .map(name => {
+        return {
+          name,
+          description: '',
+          parameterSpec: connectors[name].parameterSpec
+        }
+      })
+      .sort((x, y) => {
+        return x.name < y.name ? -1 : 1
+      })
 
     return {
       items,
--- server/graphql/resolvers/task-type/task-types.ts
+++ server/graphql/resolvers/task-type/task-types.ts
@@ -3,13 +3,17 @@ import { TaskRegistry } from '../../../engine/task-registry'
 export const taskTypesResolver = {
   taskTypes(_: any, {}, context: any) {
     var taskTypes = TaskRegistry.getTaskHandlers()
-    var items = Object.keys(taskTypes).map(name => {
-      return {
-        name,
-        description: '',
-        parameterSpec: (taskTypes[name] as any).parameterSpec
-      }
-    })
+    var items = Object.keys(taskTypes)
+      .map(name => {
+        return {
+          name,
+          description: '',
+          parameterSpec: (taskTypes[name] as any).parameterSpec
+        }
+      })
+      .sort((x, y) => {
+        return x.name < y.name ? -1 : 1
+      })
 
     return {
       items,
