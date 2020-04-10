d89776500f26bb2c32e4abcfbc6307c9deed0371
Hearty Oh <heartyoh@hatiolab.com>
2020-04-10 18:41:27 +0900

    mqtt start

---
+++ server/engine/task/mqtt-subscribe.ts
@@ -0,0 +1,29 @@
+import { TaskRegistry } from '../task-registry'
+import { Connections } from '../connections'
+
+async function MqttSubscribe(step, { logger }) {
+  const {
+    connection: connectionName,
+    params: { topic }
+  } = step
+
+  const client = Connections.getConnection(connectionName)
+
+  await client.subscribe(topic)
+
+  logger.info(`mqtt-subscribe :\n'${message.toString()}'`)
+
+  return {
+    data: message
+  }
+}
+
+MqttSubscribe.parameterSpec = [
+  {
+    type: 'string',
+    name: 'topic',
+    label: 'topic'
+  }
+]
+
+TaskRegistry.registerTaskHandler('mqtt-subscribe', MqttSubscribe)
