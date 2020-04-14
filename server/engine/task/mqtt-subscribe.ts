1dd223a881c110a0526f06810f596b529cf548bd
Hearty Oh <heartyoh@hatiolab.com>
2020-04-14 19:09:02 +0900

    mqtt pub/sub task

--- server/engine/task/mqtt-subscribe.ts
+++ server/engine/task/mqtt-subscribe.ts
@@ -1,20 +1,71 @@
 import { TaskRegistry } from '../task-registry'
 import { Connections } from '../connections'
+import { sleep } from '@things-factory/utils'
 
 async function MqttSubscribe(step, { logger }) {
   const {
     connection: connectionName,
     params: { topic },
+    subscriber
   } = step
 
-  const client = Connections.getConnection(connectionName)
+  const broker = Connections.getConnection(connectionName)
 
-  await client.subscribe(topic)
+  /*
+   * 1. subscriber list에서 subscriber를 찾는다. 없으면, 생성한다.
+   * 2. client.once(...)로 메시지를 취한다.
+   */
+  if (!subscriber) {
+    try {
+      step.subscriber = await broker.subscribe(topic)
+      logger.info(`success subscribing topic '${topic}'`)
 
-  // logger.info(`mqtt-subscribe :\n'${message.toString()}'`)
+      var TOPIC = null
+      var MESSAGE = null
+
+      step.pub = async (topic, message) => {
+        TOPIC = topic
+        MESSAGE = message
+
+        while (TOPIC) {
+          await sleep(100)
+        }
+      }
+
+      step.sub = async () => {
+        while (!TOPIC) {
+          await sleep(100)
+        }
+
+        var topic = TOPIC
+        var message = MESSAGE
+
+        TOPIC = null
+        MESSAGE = null
+
+        return {
+          topic,
+          message
+        }
+      }
+
+      broker.on('message', async (topic2, message) => {
+        if (topic !== topic2) {
+          return
+        }
+
+        await step.pub(topic2, message)
+        logger.info(`mqtt-subscribe :\n'${message.toString()}'`)
+      })
+    } catch (e) {
+      logger.error(e)
+    }
+  }
+
+  var { message } = await step.sub()
 
   return {
-    // data: message
+    data: message
   }
 }
 
@@ -22,8 +73,8 @@ MqttSubscribe.parameterSpec = [
   {
     type: 'string',
     name: 'topic',
-    label: 'topic',
-  },
+    label: 'topic'
+  }
 ]
 
 TaskRegistry.registerTaskHandler('mqtt-subscribe', MqttSubscribe)
