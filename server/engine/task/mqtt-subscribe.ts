af96f67278eb6a8cc98ef8da49192365bf4ae611
Hearty Oh <heartyoh@hatiolab.com>
2020-04-16 14:28:28 +0900

    mqtt-subscribe

--- server/engine/task/mqtt-subscribe.ts
+++ server/engine/task/mqtt-subscribe.ts
@@ -2,46 +2,51 @@ import { TaskRegistry } from '../task-registry'
 import { Connections } from '../connections'
 import { sleep } from '@things-factory/utils'
 
-async function MqttSubscribe(step, { logger }) {
+function convertDataFormat(data, format) {
+  if (format == 'json') {
+    return JSON.parse(data)
+  }
+
+  return data
+}
+
+async function MqttSubscribe(step, context) {
   const {
     connection: connectionName,
-    params: { topic },
-    subscriber
+    params: { topic, dataFormat },
+    name
   } = step
 
+  const { logger, __mqtt_subscriber } = context
+  if (!__mqtt_subscriber) {
+    context.__mqtt_subscriber = {}
+  }
+
   const broker = Connections.getConnection(connectionName)
+
+  if (!topic) {
+    throw Error(`topic is not found for ${connectionName}`)
+  }
 
   /*
    * 1. subscriber list에서 subscriber를 찾는다. 없으면, 생성한다.
    * 2. client.once(...)로 메시지를 취한다.
    */
-  if (!subscriber) {
+  if (!context.__mqtt_subscriber[name]) {
     try {
-      step.subscriber = await broker.subscribe(topic)
+      await broker.subscribe(topic)
       logger.info(`success subscribing topic '${topic}'`)
 
-      var TOPIC = null
-      var MESSAGE = null
+      var TOPIC = []
+      var MESSAGE = []
 
-      step.pub = async (topic, message) => {
-        TOPIC = topic
-        MESSAGE = message
-
-        while (TOPIC) {
-          await sleep(100)
-        }
-      }
-
-      step.sub = async () => {
-        while (!TOPIC) {
+      context.__mqtt_subscriber[name] = async () => {
+        while (MESSAGE.length == 0) {
           await sleep(100)
         }
 
-        var topic = TOPIC
-        var message = MESSAGE
-
-        TOPIC = null
-        MESSAGE = null
+        var topic = TOPIC.shift()
+        var message = MESSAGE.shift()
 
         return {
           topic,
@@ -49,12 +54,14 @@ async function MqttSubscribe(step, { logger }) {
         }
       }
 
-      broker.on('message', async (topic2, message) => {
-        if (topic !== topic2) {
+      broker.on('message', async (messageTopic, message) => {
+        if (topic !== messageTopic) {
           return
         }
 
-        await step.pub(topic2, message)
+        TOPIC.push(topic)
+        MESSAGE.push(convertDataFormat(message, dataFormat))
+
         logger.info(`mqtt-subscribe :\n'${message.toString()}'`)
       })
     } catch (e) {
@@ -62,7 +69,7 @@ async function MqttSubscribe(step, { logger }) {
     }
   }
 
-  var { message } = await step.sub()
+  var { message } = await context.__mqtt_subscriber[name]()
 
   return {
     data: message
@@ -74,6 +81,23 @@ MqttSubscribe.parameterSpec = [
     type: 'string',
     name: 'topic',
     label: 'topic'
+  },
+  {
+    type: 'select',
+    label: 'data-format',
+    name: 'dataFormat',
+    property: {
+      options: [
+        {
+          display: 'Plain Text',
+          value: 'text'
+        },
+        {
+          display: 'JSON',
+          value: 'json'
+        }
+      ]
+    }
   }
 ]
 
