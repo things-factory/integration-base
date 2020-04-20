ad4b84214d05153ed44a2126770c878c6a14452f
Hearty Oh <heartyoh@hatiolab.com>
2020-04-20 21:33:36 +0900

    Update http-post.ts

--- server/engine/task/http-post.ts
+++ server/engine/task/http-post.ts
@@ -4,9 +4,11 @@ import { GET_AUTH_HEADERS } from './http-auth'
 import fetch from 'node-fetch'
 import { URL } from 'url'
 
-async function HttpPost(step, { logger }) {
+async function HttpPost(step, { logger, data }) {
   var { connection: connectionName, params: stepOptions } = step
-  var { headers, params = {}, body, path } = stepOptions || {}
+  var { headers, params = {}, body: bodyOptions, path } = stepOptions || {}
+  var { kind = 'none', accessor } = bodyOptions || {}
+
   var connection = Connections.getConnection(connectionName)
 
   if (!connection) {
@@ -18,12 +20,29 @@ async function HttpPost(step, { logger }) {
   var url = new URL(path, endpoint)
   // Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
 
+  var body
+  var bodyData
+  if (accessor) {
+    bodyData = data[accessor]
+  }
+
+  switch (kind) {
+    case 'raw':
+    case 'x-www-form-urlencoded':
+    case 'form-data':
+      body = JSON.stringify(bodyData)
+      break
+    case 'none':
+    default:
+  }
+
   var response = await fetch(url, {
     method: 'POST',
     headers: {
       ...(GET_AUTH_HEADERS(connectionParams) || {})
       // ...headers
-    }
+    },
+    body
   })
 
   // TODO follow the format
