c46b2d453df3af45c005d2ae8320fca1ab4c6c0b
Murky <moosung.gil@hatiolab.com>
2020-05-15 15:00:39 +0900

    add node-red flow api

--- server/entities/index.ts
+++ server/entities/index.ts
@@ -1,7 +1,8 @@
 import { Connection } from './connection'
 import { Scenario } from './scenario'
+import { ScenarioFlow } from './scenario-flow'
 import { Step } from './step'
 
-export const entities = [Connection, Scenario, Step]
+export const entities = [Connection, Scenario, ScenarioFlow, Step]
 
-export { Connection, Scenario, Step }
+export { Connection, Scenario, ScenarioFlow, Step }
--- server/routes.ts
+++ server/routes.ts
@@ -1,6 +1,91 @@
+import koaBodyParser from 'koa-bodyparser'
+import { ScenarioFlow } from './entities/scenario-flow'
+import { secureRouter } from '@things-factory/auth-base/dist-server/router'
+
+import { getRepository } from 'typeorm'
+
 process.on('bootstrap-module-history-fallback' as any, (app, fallbackOption) => {
-  // var paths = ['xxxx']
-  // fallbackOption.whiteList.push(`^\/(${paths.join('|')})($|[/?#])`)
+  var paths = ['get-flows', 'save-flows']
+  fallbackOption.whiteList.push(`^\/(${paths.join('|')})($|[/?#])`)
 })
 
-process.on('bootstrap-module-route' as any, (app, routes) => {})
+process.on('bootstrap-module-route' as any, (app, router) => {
+  const bodyParserOption = {
+    formLimit: '10mb',
+    jsonLimit: '10mb',
+    textLimit: '10mb'
+  }
+
+  secureRouter.get('/get-flows/:domainId', async (context, next) => {
+    const { user } = context.state
+
+    if (!user) {
+      context.status = 403
+      context.body = {
+        success: false,
+        message: 'unauthorized'
+      }
+
+      return
+    }
+
+    const { domainId } = context.params
+    const flowRepo = getRepository(ScenarioFlow)
+    var flow: Partial<ScenarioFlow> = await flowRepo.findOne({
+      where: {
+        domain: domainId
+      }
+    })
+
+    if (!flow)
+      flow = {
+        domain: domainId,
+        flow: null
+      }
+
+    context.body = {
+      success: true,
+      flow: JSON.parse(flow.flow)
+    }
+  })
+
+  secureRouter.post('/save-flows/:domainId', koaBodyParser(bodyParserOption), async (context, next) => {
+    const { user } = context.state
+
+    if (!user) {
+      context.status = 403
+      context.body = {
+        success: false,
+        message: 'unauthorized'
+      }
+
+      return
+    }
+
+    const { domainId } = context.params
+    const { flows } = context.request.body
+    const flowRepo = getRepository(ScenarioFlow)
+
+    var flowJson = JSON.stringify(flows)
+
+    var originFlow: Partial<ScenarioFlow> = await flowRepo.findOne({
+      where: {
+        domain: domainId
+      }
+    })
+
+    if (!originFlow)
+      originFlow = {
+        domain: domainId
+      }
+
+    originFlow.flow = flowJson
+
+    const flow = await flowRepo.save(originFlow)
+
+    context.body = {
+      success: true,
+      flow: flow || []
+    }
+  })
+})
