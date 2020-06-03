a3c9d3a474bc8d1e58355ff5c4134afabe67f476
Murky <moosung.gil@hatiolab.com>
2020-06-03 15:34:46 +0900

    separate node-red-integration

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -1,8 +1,1 @@
-import { store } from '@things-factory/shell'
-import integration from './reducers/integration'
 
-export default function bootstrap() {
-  store.addReducers({
-    integration
-  })
-}
--- server/routes.ts
+++ server/routes.ts
@@ -1,10 +1,6 @@
-import { secureRouter } from '@things-factory/auth-base'
-import { IntegrationRouter } from './routers/integration-router'
 process.on('bootstrap-module-history-fallback' as any, (app, fallbackOption) => {
   var paths = ['modeller', 'integration']
   fallbackOption.whiteList.push(`^\/(${paths.join('|')})($|[/?#])`)
 })
 
-process.on('bootstrap-module-route' as any, (app, router) => {
-  secureRouter.use('/integration', IntegrationRouter.routes(), IntegrationRouter.allowedMethods())
-})
+process.on('bootstrap-module-route' as any, (app, router) => {})
